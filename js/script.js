2af584753e089be7acbd13ebf64380b6081ca4b6
Serhii Melnyk <serrmelnyk@gmail.com>
2022-10-04 18:07:45 +0300

    Block №3 + адаптация + js
    - js (Таби) 50%

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ const swiper = new Swiper('.slider-main-block', {
 		prevEl: '.body-main-block__arrow.swiper-button-prev',
 	},
 });
+
+// Таби
+
+const tabNavItems = document.querySelectorAll('.tabs-deals__buttom');
+const tabItems = document.querySelectorAll('.item-tabs');
+
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	if (targetElement.closest('.tabs-deals__buttom')) {
+		tabNavItems.forEach((tabNavItems, index) => {
+
+			// tabNavItems.classList.remove('active');
+		});
+	}
+});
